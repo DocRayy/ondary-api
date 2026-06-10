2f1f0d1b4c27bd87b1fa0e27ea4ff37e0b81ba3e
DocRayy <rayyyksr11@gmail.com>
2026-06-10 21:09:15 +0700

    fix CORS

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -6,7 +6,7 @@ export async function createApp() {
   const app = await NestFactory.create(AppModule);
   const corsOrigins = (
     process.env.CORS_ORIGINS ??
-    'http://localhost:4200,https://ondary.web.id,https://ondary.netlify.app'
+    'http://localhost:4200,http://ondary.web.id,https://ondary.web.id,https://ondary.netlify.app'
   )
     .split(',')
     .map((origin) => origin.trim())
